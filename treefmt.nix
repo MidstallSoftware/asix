31303a2eb9ee8b4504baa3fa5ae0bf08bc1268cc
Tristan Ross <tristan.ross@determinate.systems>
2026-04-26 15:44:09 -0700

    fix(treefmt): risc-v eval

--- treefmt.nix
+++ treefmt.nix
@@ -3,9 +3,9 @@
   projectRootFile = "flake.nix";
 
   programs = {
-    # NOTE: actionlint is broken on Darwin
-    actionlint.enable = !pkgs.stdenv.hostPlatform.isDarwin;
+    # NOTE: actionlint is broken on Darwin and does not support RISC-V
+    actionlint.enable = !pkgs.stdenv.hostPlatform.isDarwin && !pkgs.stdenv.hostPlatform.isRiscV;
     black.enable = true;
-    nixfmt.enable = true;
+    nixfmt.enable = !pkgs.stdenv.hostPlatform.isRiscV;
   };
 }
